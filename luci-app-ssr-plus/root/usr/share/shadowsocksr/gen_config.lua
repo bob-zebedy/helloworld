61b1ff3b600cc4f93e0eabcc106ca6baef8f30fd
Bob <zhang.zebedy@gmail.com>
2023-03-14 11:04:28 +0800

    luci-app-ssr-plus: gen_config: generate flow for REALITY

--- luci-app-ssr-plus/root/usr/share/shadowsocksr/gen_config.lua
+++ luci-app-ssr-plus/root/usr/share/shadowsocksr/gen_config.lua
@@ -20,7 +20,7 @@ function vmess_vless()
                         id = server.vmess_id,
                         security = (server.v2ray_protocol == "vmess" or not server.v2ray_protocol) and server.security or nil,
                         encryption = (server.v2ray_protocol == "vless") and server.vless_encryption or nil,
-                        flow = (server.xtls == '1') and (server.vless_flow or "xtls-rprx-splice") or (server.tls == '1') and server.tls_flow or nil
+                        flow = ((server.tls == '1') or (server.reality == '1')) and server.tls_flow or nil
                     }
                 }
             }
